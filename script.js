622b650d85523540149d02dbfbc9d682ac2999da
Brian Nguyen <briannguyencoco@gmail.com>
2022-06-24 10:44:18 -0500

    added changing of color when hovering boxes

--- script.js
+++ script.js
@@ -1,5 +1,6 @@
 //nested for loop to create all the divs needed 
 // use flexbox to make them display in rows and wrap if no space on line
+//adds divs to the container 
 const container = document.querySelector('#container');
 for(let i = 0; i < 16; i++) {
     for(let j = 0; j < 16; j++) {
@@ -10,3 +11,12 @@ for(let i = 0; i < 16; i++) {
         
     }
 }
+
+//make nodelist of all boxes
+//use .forEach() to loop through list and apply eventlistener
+//if "mouseover", then add background-color: black;
+const boxes = document.querySelectorAll('.box');
+
+boxes.forEach((box) => {
+    box.addEventListener('mouseover', () => box.style.backgroundColor = 'blue');
+});
