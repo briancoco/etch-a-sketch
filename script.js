b85239d23384ae9ab321a8f97d1ecf1fb6d7cd9d
Brian Nguyen <briannguyencoco@gmail.com>
2022-06-24 16:17:25 -0500

    working grid sizer and color changer

--- script.js
+++ script.js
@@ -9,22 +9,42 @@ for(let i = 0; i < 4; i++) {
         const div = document.createElement('div');
         div.className = 'box';
         div.style.border = "1px solid black"
+        div.addEventListener('mouseover', () => div.style.backgroundColor = 'blue');
         container.appendChild(div);
         
     }
 }
+function removeBoxes() {
+    while(container.firstChild) {
+        container.removeChild(container.firstChild);
+    }
+}
+
+
+function changeGridSize() {
+    let size = prompt('Enter Grid Size: ');
+    removeBoxes();
+
+    for(let i = 0; i < parseInt(size); i++) {
+        for(let j = 0; j < parseInt(size); j++) {
+            const div = document.createElement('div');
+            div.className = 'box';
+            div.style.border = "1px solid black"
+            div.addEventListener('mouseover', () => div.style.backgroundColor = 'blue');
+            container.appendChild(div);
+            
+        }
+    }
+    container.style.cssText = `grid-template-columns : repeat(${size}, 1fr); grid-template-rows: repeat(${size}, 1fr);`;
+}
+
+
 
 //make nodelist of all boxes
 //use .forEach() to loop through list and apply eventlistener
 //if "mouseover", then add background-color: black;
 //have to put boxes referece here because boxes arent made until the loop is ran.
-const boxes = document.querySelectorAll('.box');
-boxes.forEach((box) => {
-    box.addEventListener('mouseover', () => box.style.backgroundColor = 'blue');
-});
 
-function removeBoxes() {
-    boxes.forEach((box) => container.removeChild(box));
-}
 
-gridSize.addEventListener('click', removeBoxes);
+
+gridSize.addEventListener('click', changeGridSize);
