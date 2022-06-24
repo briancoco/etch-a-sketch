b1441f4aea06b9fb38cd304cff84ac177f461aa8
Brian Nguyen <briannguyencoco@gmail.com>
2022-06-24 15:03:42 -0500

    added removeBoxes()

--- script.js
+++ script.js
@@ -2,11 +2,13 @@
 // use flexbox to make them display in rows and wrap if no space on line
 //adds divs to the container 
 const container = document.querySelector('#container');
+const gridSize = document.querySelector('.grid-size');
+
 for(let i = 0; i < 4; i++) {
     for(let j = 0; j < 4; j++) {
         const div = document.createElement('div');
         div.className = 'box';
-        div.style.border = "2px solid black"
+        div.style.border = "1px solid black"
         container.appendChild(div);
         
     }
@@ -15,8 +17,14 @@ for(let i = 0; i < 4; i++) {
 //make nodelist of all boxes
 //use .forEach() to loop through list and apply eventlistener
 //if "mouseover", then add background-color: black;
+//have to put boxes referece here because boxes arent made until the loop is ran.
 const boxes = document.querySelectorAll('.box');
-
 boxes.forEach((box) => {
     box.addEventListener('mouseover', () => box.style.backgroundColor = 'blue');
 });
+
+function removeBoxes() {
+    boxes.forEach((box) => container.removeChild(box));
+}
+
+gridSize.addEventListener('click', removeBoxes);
